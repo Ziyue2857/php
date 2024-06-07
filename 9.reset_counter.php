fca4537251d0dbbcb0e6f0c114ddacdf2542c011
范瑞容 <167466695+Ziyue2857@users.noreply.github.com>
2024-06-07 10:24:16 +0800

    9.reset_counter.php

--- 9.reset_counter.php
+++ 9.reset_counter.php
@@ -1,7 +1,8 @@
 <?php
     session_start();
-    unset($_SESSION["counter"]);
+    unset($_SESSION["counter"]);// 移除 session 中的 counter 變數
     echo "counter重置成功....";
     echo "<meta http-equiv=REFRESH content='2; url=8.counter.php'>";
 
 ?>
+<!--移除變數，通常是回歸原始的網頁計算模式>
